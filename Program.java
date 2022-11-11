00eda076dfd4d5db412cfb84d5854cc7bf5fe94c
Pedro Penna <pepenna@squaretrade.com>
2022-11-11 10:32:45 -0300

    Implement first attempt to solve the problem

--- Program.java
+++ Program.java
@@ -5,8 +5,28 @@ import java.util.Set;
 public class Program {
     public List<Integer[]> findQuadruplets(int[] array, int targetSum) {
         Set<Set<Integer>> results = new HashSet<>();
-//        foo(array, results, targetSum);
+        Set<Integer> selected = new HashSet<>();
+        find(array, 0, selected, results, targetSum);
+
+        System.out.println(results);
         return null;
+    }
+
+    private void find(final int[] array, int startingIndex, final Set<Integer> selected, final Set<Set<Integer>> results, final int targetSum) {
+//System.out.println("find");
+        final Integer sum = selected.stream()
+                .reduce(0, (a, b) -> a + b);
+//System.out.println("sum: " + sum);
+        if (targetSum == sum) {
+//System.out.println("target reached!");
+            results.add(selected);
+        }
+
+        for (int i = startingIndex; i < array.length; i++) {
+            Set<Integer> newSet = new HashSet<>(selected);
+            newSet.add(array[i]);
+            find(array, startingIndex + 1, newSet, results, targetSum);
+        }
     }
 
     public static void main(String[] args) {
