7c4296cc27f5102f17ffec5f33b74bfd2eb1fe2f
Pedro Penna <pepenna@squaretrade.com>
2022-11-11 10:39:03 -0300

    Add debugging messages for the solutions

--- Program.java
+++ Program.java
@@ -1,6 +1,8 @@
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Program {
     public List<Integer[]> findQuadruplets(int[] array, int targetSum) {
@@ -8,8 +10,10 @@ public class Program {
         Set<Integer> selected = new HashSet<>();
         find(array, 0, selected, results, targetSum);
 
-        System.out.println(results);
-        return null;
+System.out.println("Result set: " + results);
+        return results.stream()
+                .map(integers -> integers.toArray(new Integer[0]))
+                .collect(Collectors.toList());
     }
 
     private void find(final int[] array, int startingIndex, final Set<Integer> selected, final Set<Set<Integer>> results, final int targetSum) {
@@ -34,6 +38,9 @@ public class Program {
 
         final List<Integer[]> quadruplets = program.findQuadruplets(new int[]{7, 6, 4, -1, 1, 2}, 16);
 
-        System.out.println(quadruplets);
+        System.out.println("Solution");
+        for (Integer[] solutionArray : quadruplets) {
+            System.out.println(Arrays.asList(solutionArray));
+        }
     }
 }
